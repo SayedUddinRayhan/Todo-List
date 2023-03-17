31b88e1720bfb9eb553517fc2c39f92bb24d6f1f
Md. Sayed Uddin Rayhan <67170635+SayedUddinRayhan@users.noreply.github.com>
2023-03-17 17:37:36 +0000

    add necessary functions

---
+++ script.js
@@ -0,0 +1,26 @@
+let userInput = document.querySelector("#userInput");
+let addBtn = document.querySelector("#addBtn");
+
+let userArr = [];
+
+addBtn.onclick = () => {
+  let value = userInput.value;
+  userArr.push({'id':value});
+  console.log(userArr)
+}
+
+function saveInfo(){
+  
+}
+
+function displayInfo(){
+  
+}
+
+function editInfo(){
+  
+}
+
+function deleteInfo(){
+  
+}
