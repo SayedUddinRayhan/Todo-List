909e2049064d373adbe9ec3d18598e67e7496f3c
Md. Sayed Uddin Rayhan <67170635+SayedUddinRayhan@users.noreply.github.com>
2023-03-17 18:30:22 +0000

    worked for displaying

--- script.js
+++ script.js
@@ -1,29 +1,46 @@
 let userInput = document.querySelector("#userInput");
 let addBtn = document.querySelector("#addBtn");
+let recordsDisplay = document.querySelector("#records");
 
 let userArr = [];
 
 let objStr = localStorage.getItem('id');
-userArr = JSON.parse(objStr);
+if (objStr != null) {
+  userArr = JSON.parse(objStr);
+}
+
+displayInfo();
 addBtn.onclick = () => {
   let value = userInput.value;
-  userArr.push({'id':value});
+  if (value.length != 0) {
+    userArr.push({ 'id': value });
+  }
   saveInfo(userArr);
+  userInput.value = '';
+  displayInfo();
 }
 
-function saveInfo(arr){
+function saveInfo(arr) {
   let makeString = JSON.stringify(arr);
-  localStorage.setItem('id',makeString);
+  localStorage.setItem('id', makeString);
 }
 
-function displayInfo(){
-  
+function displayInfo() {
+  let statement = '';
+  userArr.forEach((user, i) => {
+    statement += `<tr>
+            <th scope="row">${i + 1}</th>
+            <td>${user.id}</td>
+            <td><i class="fa-solid fa-pen-to-square"></i> <i class="fa-solid fa-trash-can"></i></td>
+          </tr>`
+  })
+  recordsDisplay.innerHTML = statement;
 }
 
-function editInfo(){
-  
+function editInfo() {
+
 }
 
-function deleteInfo(){
-  
+function deleteInfo() {
+
 }
