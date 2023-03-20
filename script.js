bc51fec52d5b8bd9995469157d8f76548d24c60c
Md. Sayed Uddin Rayhan <67170635+SayedUddinRayhan@users.noreply.github.com>
2023-03-20 06:14:39 +0000

    added search option

--- script.js
+++ script.js
@@ -14,16 +14,16 @@ if (objStr != null) {
 displayInfo();
 addBtn.onclick = () => {
   let value = userInput.value;
-  if (value.length != 0){
-    if (edit_id!=null){
-    userArr.splice(edit_id,1,{ 'id': value });
-    edit_id = null;
+  if (value.length != 0) {
+    if (edit_id != null) {
+      userArr.splice(edit_id, 1, { 'id': value });
+      edit_id = null;
+    }
+    else {
+      userArr.push({ 'id': value });
+    }
   }
-  else{
-    userArr.push({ 'id': value });
-  }
-  }
-  
+
   saveInfo(userArr);
   userInput.value = '';
   addBtn.innerText = btnText;
@@ -57,3 +57,26 @@ function deleteInfo(i) {
   userArr.splice(i, 1);
   saveInfo(userArr);
 }
+
+
+let allTr = document.querySelectorAll("#records tr")
+let searchInput = document.querySelector("#search");
+
+
+searchInput.addEventListener('input', function(e) {
+  let searchStr = e.target.value.toLowerCase();
+  recordsDisplay.innerHTML = '';
+
+  allTr.forEach(tr => {
+    let tdAll = tr.querySelectorAll("td");
+    let tdStr = tdAll[0].innerText.toLowerCase();
+    if (tdStr.indexOf(searchStr) > -1) {
+      recordsDisplay.appendChild(tr);
+    }
+  });
+  if (recordsDisplay.innerHTML == '') {
+    recordsDisplay.innerHTML = "No Records Found";
+  }
+
+
+})
